ba7f673a36fe3ca8986de6fc5b6c23d22e0fd36e
MeruemShen <dorian1711x@gmail.com>
2024-12-10 14:50:55 +0100

    add delete/getId Blogs & change field

--- routes/blog.routes.js
+++ routes/blog.routes.js
@@ -1,8 +1,10 @@
-const { createBlog, getBlogs } = require("../controllers/blog.controllers");
+const { createBlog, getBlogs, getBlogById, deleteBlog } = require("../controllers/blog.controllers");
 const express = require('express');
 const router = express.Router();
 
 router.post('/', createBlog);
 router.get('/', getBlogs);
+router.get('/:id', getBlogById); 
+router.delete('/:id', deleteBlog);  
 
 module.exports = router;
